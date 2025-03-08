1e9e12678e4d92a8a81bd6144ef21dd287d81481
Vinod Nerella <vinod1502506@mastek.com>
2025-03-08 11:31:22 +0530

    added comments

--- MyFirstWebAPIProject/Deployment/terraform_conditional_run.ps1
+++ MyFirstWebAPIProject/Deployment/terraform_conditional_run.ps1
@@ -9,18 +9,24 @@ if (-not $ResourceGroupName -or -not $Location -or -not $TerraformDirectory) {
     Write-Error "ResourceGroupName, Location, and TerraformDirectory parameters are required."
     exit 1
 }
-
+Write-Output "This text is printing1"
 # Login to Azure
 az login --service-principal -u $env:ARM_CLIENT_ID -p $env:ARM_CLIENT_SECRET --tenant $env:ARM_TENANT_ID
+Write-Output "This text is printing2"
 
 # Set the subscription
 az account set --subscription $env:ARM_SUBSCRIPTION_ID
 
+Write-Output "This text is printing3"
+
 # Navigate to the Terraform directory
 cd $TerraformDirectory
+Write-Output "This text is printing4"
 
 # Initialize Terraform
 terraform init
+
+Write-Output "This text is printing5"
 
 # Plan Terraform deployment
 terraform plan -out=tfplan
