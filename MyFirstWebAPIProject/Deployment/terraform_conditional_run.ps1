16292f610526accbfa8de6673551a6c979137aa2
Vinod Nerella <vinod1502506@mastek.com>
2025-03-08 12:14:21 +0530

    imported existing azure resounces to state file

--- MyFirstWebAPIProject/Deployment/terraform_conditional_run.ps1
+++ MyFirstWebAPIProject/Deployment/terraform_conditional_run.ps1
@@ -15,6 +15,18 @@ Write-Output "This text is printing2"
 # Initialize Terraform
 terraform init
 
+# Import the existing resource group
+terraform import azurerm_resource_group.example /subscriptions/c3d246d3-988a-45ae-ba70-f7faac1e2d0a/resourceGroups/$ResourceGroupName
+
+# Import the existing container app environment
+terraform import azurerm_container_app_environment.example /subscriptions/c3d246d3-988a-45ae-ba70-f7faac1e2d0a/resourceGroups/$ResourceGroupName/providers/Microsoft.Web/kubeEnvironments/test-example-environmentvin
+
+# Import the existing container registry
+terraform import azurerm_container_registry.example /subscriptions/c3d246d3-988a-45ae-ba70-f7faac1e2d0a/resourceGroups/$ResourceGroupName/providers/Microsoft.ContainerRegistry/registries/testexampleregistryvin
+
+# Import the existing storage account
+terraform import azurerm_storage_account.example /subscriptions/c3d246d3-988a-45ae-ba70-f7faac1e2d0a/resourceGroups/$ResourceGroupName/providers/Microsoft.Storage/storageAccounts/teststorageaccount12345v
+
 # Plan Terraform deployment
 terraform plan -out=tfplan
 
